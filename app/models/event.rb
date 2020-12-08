38afe46f629219d2cc160124246699cd87651780
sevab1 <starolesjev@yandex.ru>
2020-12-08 19:44:04 +0400

    bootstrap+bbq

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,11 @@
 class Event < ApplicationRecord
+  # Событие принадлежит юзеру
+  belongs_to :user
+
+  # Валидируем юзера на присутствие. В Rails 5 связи
+  # валидируются по умолчанию
+  validates :user, presence: true
+
   # Валидируем заголовок, он не может быть длиннее 255 букв
   validates :title, presence: true, length: {maximum: 255}
   # У события должны быть заполнены место и время
