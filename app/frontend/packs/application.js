61bcfc53cdd73ae2413dd8e09a4db7add9934d6f
sevab1 <starolesjev@yandex.ru>
2020-12-24 11:46:12 +0400

    github upload

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -1,6 +1,9 @@
 require("@rails/ujs").start()
 
 import 'bootstrap/dist/js/bootstrap'
-import '../styles/application.css'
+import '../styles/application.scss'
+// import '../styles/users.scss'
+import '../styles/events.scss'
+
 
 const images = require.context('../images', true)
